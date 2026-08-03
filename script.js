b6ecb8da86faa17c70ac7ba200c94876dcda48cc
sailentwisdom <mksahuji1999@gmail.com>
2026-08-03 23:10:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+import { db, collection, getDocs } from "./firebase.js";
 const searchBox = document.getElementById("searchBox");
 
 searchBox.addEventListener("keyup", function(){
@@ -40,3 +41,29 @@ text:text
 });
 
 }
+async function loadQuotes(){
+
+const querySnapshot = await getDocs(collection(db, "quotes"));
+
+const quotesSection = document.querySelector(".quotes");
+
+querySnapshot.forEach((doc)=>{
+
+let data = doc.data();
+
+let div = document.createElement("div");
+
+div.className = "quote";
+
+div.innerHTML = `
+<p>${data.quote}</p>
+<span>— SilentWisdom</span>
+`;
+
+quotesSection.appendChild(div);
+
+});
+
+}
+
+loadQuotes();
