70b455e7bc337cc1a6d20e902ccb9fd0a8cc602c
sailentwisdom <mksahuji1999@gmail.com>
2026-08-03 22:40:31 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-alert("Welcome to Silent Wisdom ❤️");
+const searchBox = document.getElementById("searchBox");
+
+searchBox.addEventListener("keyup", function(){
+
+let value = searchBox.value.toLowerCase();
+
+let quotes = document.querySelectorAll(".quote");
+
+quotes.forEach(function(quote){
+
+let text = quote.innerText.toLowerCase();
+
+if(text.includes(value)){
+    quote.style.display = "block";
+}
+else{
+    quote.style.display = "none";
+}
+
+});
+
+});
